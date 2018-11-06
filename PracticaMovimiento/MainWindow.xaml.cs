680f14e54cb48ea4aa1a6483637bffbbb952aca3
manuel ernesto alvarez figueroa <manuel123317@hotmail.com>
2018-11-06 08:29:47 -0700

    Se mueve en todas las dirreciones y hay enemigos

--- PracticaMovimiento/MainWindow.xaml.cs
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
 
 namespace PracticaMovimiento
 {
@@ -20,18 +22,68 @@ namespace PracticaMovimiento
     /// </summary>
     public partial class MainWindow : Window
     {
+        Stopwatch stopwatch;
+        TimeSpan tiempoAnterior;
+
         public MainWindow()
         {
             InitializeComponent();
             miCanvas.Focus();
-        }
 
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+            // 1. Establecer Instrucciones
+            ThreadStart threadStart = new ThreadStart(MoverRanas);
+            // 2. Inicializar el thread
+            Thread threadMoverEnemigos = new Thread(threadStart);
+            // 3. Ejecutar el thread
+            threadMoverEnemigos.Start();
+
+        }
+        void MoverRanas()
+        {
+            while (true)
+            {
+                Dispatcher.Invoke(
+                () =>
+                {
+                    var TiempoActual = stopwatch.Elapsed;
+                    var DeltaTime = TiempoActual - tiempoAnterior;
+                        double leftCarroActual = Canvas.GetLeft(ranita);
+                        Canvas.SetLeft(ranita, leftCarroActual - (140*DeltaTime.TotalSeconds));
+                    if( Canvas.GetLeft(ranita) <= -100)
+                    {
+                        Canvas.SetLeft(ranita,1000);
+                    }
+                    tiempoAnterior = TiempoActual;
+                }
+                );
+            }
+        }
         private void miCanvas_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Up)
+            if (e.Key == Key.Right)
             {
                 double carritoderecha = Canvas.GetLeft(carrito);
-                Canvas.SetLeft(carrito, carritoderecha + 15);
+                Canvas.SetLeft(carrito, carritoderecha + 50);
+            }
+
+            if (e.Key == Key.Left)
+            {
+                double carritoderecha = Canvas.GetLeft(carrito);
+                Canvas.SetLeft(carrito, carritoderecha - 50);
+            }
+
+            if (e.Key == Key.Up)
+            {
+                double carritoarriba = Canvas.GetTop(carrito);
+                Canvas.SetTop(carrito, carritoarriba - 50);
+            }
+            if (e.Key == Key.Down)
+            {
+                double carritoarriba = Canvas.GetTop(carrito);
+                Canvas.SetTop(carrito, carritoarriba + 50);
             }
         }
     }
